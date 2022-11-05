2755c955926011d704e73d797676ca7c883bb9c2
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-11-05 08:48:47 +0100

    fixed load more

---
+++ src/components/notes/StartMessage.jsx
@@ -0,0 +1,8 @@
+import { StartImageWrapper } from './StartMessage.styled';
+
+export const StartMessage = () => (
+  <StartImageWrapper>
+    <p>Please, find a picture</p>
+    <img alt="cat" src="./images/cat.jpg" />
+  </StartImageWrapper>
+);
---
+++ src/components/notes/StartMessage.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StartImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  width: 30%;
+`;
